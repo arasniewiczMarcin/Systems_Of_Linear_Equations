718c8143f35c10a0f0877859b62d4a43d8ed71ac
arasniewiczMarcin <arasniewicz.marcin@gmail.com>
2024-05-01 17:03:33 +0200

    Finished project

--- .venv/Scripts/Solving_Methods.py
+++ .venv/Scripts/Solving_Methods.py
@@ -18,7 +18,7 @@ class SolvingMethods:
 
 
     @staticmethod
-    def gauss_seidl(A, n, m, b):
+    def gauss_seidl(A, n, m, b, max_iterations):
         start = time.time()
         err_norm = 10 ** -9
         f = 8
@@ -27,7 +27,7 @@ class SolvingMethods:
         error = 1
         iterations = -1
         res_norm = []
-        while error > err_norm:
+        while error > err_norm and iterations < max_iterations:
             iterations += 1
             for i in range(n):
                 sum1 = sum(A.matrix[i][j] * x_new.matrix[j][0] for j in range(i))
@@ -45,7 +45,7 @@ class SolvingMethods:
         return res_norm, iterations, gauss_seidl_time
 
     @staticmethod
-    def jacobi(A, n, m, b):
+    def jacobi(A, n, m, b, max_iterations):
         start = time.time()
         err_norm = 10 ** -9
         f = 8
@@ -54,7 +54,7 @@ class SolvingMethods:
         error = 1
         iterations = -1
         res_norm = []
-        while error > err_norm:
+        while error > err_norm and iterations < max_iterations:
             iterations += 1
             for i in range(n):
                 sum1 = sum(A.matrix[i][j] * x.matrix[j][0] for j in range(i))
@@ -72,3 +72,37 @@ class SolvingMethods:
         return res_norm, iterations, jacobi_time
 
 
+    @staticmethod
+    def lu_factoring(A, n, m, b):
+        start = time.time()
+        U = copy.deepcopy(A)
+        L = Matrix(n, n, 0, 0, 0)
+        for i in range(n):
+            L.matrix[i][i] = 1
+
+        for i in range(2, m + 1):
+            for j in range(1, i):
+                L.matrix[i - 1][j - 1] = U.matrix[i - 1][j - 1] / U.matrix[j - 1][j - 1]
+                for t in range(n):
+                    U.matrix[i - 1][t] = U.matrix[i - 1][t] - L.matrix[i - 1][j - 1] * U.matrix[j - 1][t]
+
+        x = Matrix(1, n, 0, 0, 0)
+        y = Matrix(1, n, 0, 0, 0)
+        for i in range(n):
+            y.matrix[i][0] = b.matrix[i][0]
+            for j in range(i):
+                y.matrix[i][0] = y.matrix[i][0] - L.matrix[i][j] * y.matrix[j][0]
+
+        for i in range(n - 1, -1, -1):
+            x.matrix[i][0] = y.matrix[i][0]
+            for j in range(i + 1, n):
+                x.matrix[i][0] = x.matrix[i][0] - U.matrix[i][j] * x.matrix[j][0]
+            x.matrix[i][0] = x.matrix[i][0] / U.matrix[i][i]
+
+        res = A.matmul(x)
+        res = res - b
+        error = SolvingMethods.euclidean_norm(res)
+        stop = time.time()
+        lu_time = stop - start
+
+        return error, lu_time
--- .venv/Scripts/main.py
+++ .venv/Scripts/main.py
@@ -7,23 +7,25 @@ from plot import Plot
 
 
 if __name__ == "__main__":
-    n = 957
     e = 8
+
+    n = 957
     a1 = 5 + e
     a2 = a3 = -1
     f = 8
     b = [np.sin(i * (f + 1)) for i in range(n)]
+
     vector_b = Matrix(1, n, 0, 0, 0)
     vector_b.assign_values_to_vector(b)
     A = Matrix(n, n, a1, a2, a3)
 
-    #jacobi_res_norm, jacobi_iterations, jacobi_time = SolvingMethods.jacobi(A, n, n, vector_b)
-    #print(f"Jacobi: {jacobi_iterations} iterations, {jacobi_time} seconds")
+    jacobi_res_norm, jacobi_iterations, jacobi_time = SolvingMethods.jacobi(A, n, n, vector_b, 200)
+    print(f"Jacobi: {jacobi_iterations} iterations, {jacobi_time} seconds")
 
-    #gauss_res_norm, gauss_iterations, gauss_time = SolvingMethods.gauss_seidl(A, n, n, vector_b)
-    #print(f"Gauss-Seidel: {gauss_iterations} iterations, {gauss_time} seconds")
+    gauss_res_norm, gauss_iterations, gauss_time = SolvingMethods.gauss_seidl(A, n, n, vector_b, 200)
+    print(f"Gauss-Seidel: {gauss_iterations} iterations, {gauss_time} seconds")
 
-    #Plot.make_plot_norms(jacobi_iterations, jacobi_res_norm, gauss_iterations, gauss_res_norm)
+    Plot.make_plot_norms(jacobi_iterations, jacobi_res_norm, gauss_iterations, gauss_res_norm)
 
     #equation number 2
     a1 = 3
@@ -35,8 +37,40 @@ if __name__ == "__main__":
     vector_b.assign_values_to_vector(b)
     A = Matrix(n, n, a1, a2, a3)
 
-    jacobi_res_norm, jacobi_iterations, jacobi_time = SolvingMethods.jacobi(A, n, n, vector_b)
+    jacobi_res_norm, jacobi_iterations, jacobi_time = SolvingMethods.jacobi(A, n, n, vector_b, 100)
     print(f"Jacobi: {jacobi_iterations} iterations, {jacobi_time} seconds")
 
-    gauss_res_norm, gauss_iterations, gauss_time = SolvingMethods.gauss_seidl(A, n, n, vector_b)
+    gauss_res_norm, gauss_iterations, gauss_time = SolvingMethods.gauss_seidl(A, n, n, vector_b, 100)
     print(f"Gauss-Seidel: {gauss_iterations} iterations, {gauss_time} seconds")
+
+    Plot.make_plot_norms(jacobi_iterations, jacobi_res_norm, gauss_iterations, gauss_res_norm)
+
+    err, lu_time = SolvingMethods.lu_factoring(A, n, n, vector_b)
+    print(f"LU factorization: {lu_time} seconds") 
+    print(f"error norm = {err}")
+
+    #equation number 3
+    N = [100, 500, 1000, 1500, 2000, 2500]
+    jacobi_times = []
+    gauss_times = []
+    lu_times = []
+    for i in N:
+        e = 8
+        a1 = 5 + e
+        a2 = a3 = -1
+        f = 8
+        b = [np.sin(j * (f + 1)) for j in range(i)]
+        vector_b = Matrix(1, i, 0, 0, 0)
+        vector_b.assign_values_to_vector(b)
+        A = Matrix(i, i, a1, a2, a3)
+        jacobi_res_norm, jacobi_iterations, jacobi_time = SolvingMethods.jacobi(A, i, i, vector_b, 200)
+        print(f"For N = {i} - Jacobi: {jacobi_iterations} iterations, {jacobi_time} seconds {jacobi_res_norm} ")
+        jacobi_times.append(jacobi_time)
+        gauss_res_norm, gauss_iterations, gauss_time = SolvingMethods.gauss_seidl(A, i, i, vector_b, 200)
+        print(f"For N = {i} - Gauss-Seidel: {gauss_iterations} iterations, {gauss_time} seconds {gauss_res_norm}")
+        gauss_times.append(gauss_time)
+        err, lu_time = SolvingMethods.lu_factoring(A, i, i, vector_b)
+        print(f"For N = {i} - LU factorization: {lu_time} seconds {err}")
+        lu_times.append(lu_time)
+
+    Plot.make_plot_comparison(N, jacobi_times, gauss_times, lu_times)
